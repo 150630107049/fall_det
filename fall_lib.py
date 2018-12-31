cb825eaf1ace99d2c260dee775ce1889c4b4f15f
150630107049 <krina23997@gmail.com>
2018-12-31 10:57:06 +0530

    first commit

---
+++ fall_lib.py
@@ -0,0 +1,14 @@
+def first_module(arg):
+    print("module created")
+    return(arg)
+
+class First:
+
+    def __init__(self):
+            print("class created")
+
+    def first(self):
+        """
+            doc string for fall detection system
+        """
+        print("method call")
